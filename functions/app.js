8c41170066a754c7ebf998c78043ae58699c6f1e
aleks <kulykov@ukr.net>
2021-06-24 14:39:24 +0300

    Fix spaces, add comments

--- functions/app.js
+++ functions/app.js
@@ -10,7 +10,7 @@ context.moveTo(50, 50);      // Передвигает перо в точку (x
 context.lineTo(50, 100);       // Рисует линию до точки
 context.lineTo(100, 100);
 context.lineTo(100, 50);
-context.lineTo(50, 50);     
+context.lineTo(50, 50);
 
 context.lineWidth = 1;          // Устанавливаем толщину линии
 context.strokeStyle = 'red';  // Устанавливаем цвет
@@ -18,45 +18,46 @@ context.strokeStyle = 'red';  // Устанавливаем цвет
 context.stroke();                   // Отображает путь
 
 
-context.beginPath();        
+context.beginPath();
 
-context.moveTo(150, 50);   
+context.moveTo(150, 50);
 
 context.lineTo(150, 100);
 context.lineTo(200, 100);
 context.lineTo(200, 50);
 context.lineTo(150, 50);  
 
-context.lineWidth = 2;    
+context.lineWidth = 2;
 context.strokeStyle = 'green';
 
 
 context.stroke();
 
-context.beginPath();        
+context.beginPath();
 
-context.moveTo(250, 50);   
+context.moveTo(250, 50);
 
 context.lineTo(250, 100);
 context.lineTo(300, 100);
 context.lineTo(300, 50);
 context.lineTo(250, 50);  
 
-context.lineWidth = 3;    
+context.lineWidth = 3;
 context.strokeStyle = 'blue'; 
 
-context.stroke();      
+context.stroke();
 
 // Рисуем круги
 
-context.beginPath();
+context.beginPath();                                // Начинает новый путь
 
-context.arc(75, 175, 25, 0, 2 * Math.PI);
+context.arc(75, 175, 25, 0, 2 * Math.PI); // Рисует круг: x , y , R, fill start, fill end
 
-context.lineWidth = 1;    
+
+context.lineWidth = 1;
 context.strokeStyle = 'red'; 
 
-context.stroke();
+context.stroke();                                       // Отображает путь
 
 
 
@@ -64,7 +65,7 @@ context.beginPath();
 
 context.arc(175, 175, 25, 0, 2 * Math.PI);
 
-context.lineWidth = 2;    
+context.lineWidth = 2;
 context.strokeStyle = 'green'; 
 
 context.stroke();
@@ -74,7 +75,7 @@ context.beginPath();
 
 context.arc(275, 175, 25, 0, 2 * Math.PI);
 
-context.lineWidth = 3;    
+context.lineWidth = 3;
 context.strokeStyle = 'blue'; 
 
 context.stroke();
@@ -97,11 +98,11 @@ context.stroke();                   // Отображает путь
 
 context.beginPath();
 
-context.moveTo(175, 250);   
+context.moveTo(175, 250);
 
 context.lineTo(150, 300);
 context.lineTo(200, 300);
-context.lineTo(175, 250);  
+context.lineTo(175, 250);
 
 context.lineWidth = 2;
 context.strokeStyle = 'green';
@@ -115,7 +116,7 @@ context.moveTo(275, 250);
 
 context.lineTo(250, 300);
 context.lineTo(300, 300);
-context.lineTo(275, 250);  
+context.lineTo(275, 250);
 
 context.lineWidth = 3;
 context.strokeStyle = 'blue'; 
