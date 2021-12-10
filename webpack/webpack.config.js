edd79431e8e42baae2830b1f84c042774b3341d3
aleks <kulykov@ukr.net>
2021-12-10 17:39:54 +0200

    webpack-Init

---
+++ webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+      index: path.resolve(__dirname, "src", "main.js") 
+  },
+  output: {
+    path: path.resolve(__dirname, "dist")
+  }
+};
