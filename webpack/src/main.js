87c587a3ab16d8f77ef76041aa5cf29ab24e6795
aleks <kulykov@ukr.net>
2021-12-10 17:57:13 +0200

    webpack-add html and hmr

---
+++ webpack/src/main.js
@@ -0,0 +1,4 @@
+const newH1 = document.createElement("h1");
+newH1.innerHTML = "Привет!";
+
+document.querySelector('body').appendChild(newH1);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: {
-      index: path.resolve(__dirname, "src", "main.js") 
-  },
-  output: {
-    path: path.resolve(__dirname, "dist")
-  }
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        hot: true,
+    },
+    entry: {
+        index: path.resolve(__dirname, "src", "main.js") 
+    },
+    output: {
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+        })
+    ]
 };
