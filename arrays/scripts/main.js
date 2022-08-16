72de4c9147328bd0f019ef823c7fcc46b2c50d63
aleks <kulykov@ukr.net>
2022-08-16 20:05:07 +0300

    Base template

---
+++ arrays/scripts/main.js
@@ -0,0 +1,8 @@
+const init = function(){
+}
+
+if (document.readyState === 'loading') {  // Загрузка ещё не закончилась
+  document.addEventListener('DOMContentLoaded', init);
+} else {  // `DOMContentLoaded` Уже сработал
+  init();
+}
