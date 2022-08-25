5ed5fd1c90b171d25e7e24ffaab466cc59475367
aleks <kulykov@ukr.net>
2022-08-25 17:51:48 +0300

    split to modules

--- symbols/scripts/main.js
+++ symbols/scripts/main.js
@@ -1,14 +1,8 @@
-const MIN_RAND = 0;
-const MAX_RAND = 1000;
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-
 const init = function(){
-
+    
+    
+    
+    
 }
 
 if (document.readyState === 'loading') {  // Загрузка ещё не закончилась
