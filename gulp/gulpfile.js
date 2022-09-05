3553171e703efe1c017f73525c6bc2734d9e28b4
aleks <kulykov@ukr.net>
2022-09-05 19:09:45 +0300

    Add clean & autoinject & sourcemap

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,21 +1,33 @@
 const { src, dest, series, watch  } = require('gulp');
+const clean = require('gulp-clean');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const ejs = require('gulp-ejs');
 const less = require('gulp-less');
+const inject = require('gulp-inject');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
 //const autoprefixer = require('gulp-autoprefixer');
 
 const sync = require("browser-sync").create();
 
 const buildPath = './build/';
 
+function cleanBuild() {
+  return src(buildPath, {read: false})
+    .pipe(clean());
+}
+
 function generateJS() {
   return src('./src/js/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(dest(buildPath+'/js'))
+    //.pipe(dest(buildPath+'/js'))
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
+    //.pipe(rename({ extname: '.min.js' }))
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write(''))
     .pipe(dest(buildPath+'/js'))
     .pipe(sync.stream());
 }
@@ -30,6 +42,7 @@ function copyFavicon() {
 }
 
 function generateHTML() {
+    const sources = src([buildPath+'/js/*.js', buildPath+'/css/*.css'], {read: false});
     return src("./src/index.ejs")
         .pipe(ejs({
             title: "Gulp simple example",
@@ -37,6 +50,7 @@ function generateHTML() {
         .pipe(rename({
             extname: ".html"
         }))
+        .pipe(inject(sources, {ignorePath: 'build', addRootSlash: true}))
         .pipe(dest(buildPath))
         .pipe(sync.stream());
 }
@@ -51,7 +65,7 @@ function generateCSS() {
         .pipe(sync.stream());
 }
 
-exports.build = series(copyFavicon, generateJS, generateVendor, generateCSS, generateHTML);
+exports.build = series(cleanBuild, copyFavicon, generateJS, generateVendor, generateCSS, generateHTML);
 
 function watchFiles() {
     watch('./src/index.ejs', generateHTML);
@@ -73,7 +87,6 @@ function browserServeSync(cb) {
         }
     });
 
-    copyFavicon();
     watch('./src/index.ejs', generateHTML);
     watch('./src/css/*.less', generateCSS);
     watch('./src/css/*.css', generateCSS);
@@ -82,7 +95,7 @@ function browserServeSync(cb) {
     watch([buildPath + "**.html"]).on('change', sync.reload);
 }
 
-exports.serve = browserServeSync;
+exports.serve = series(cleanBuild, copyFavicon, generateJS, generateVendor, generateCSS, generateHTML, browserServeSync);
 
 exports.default = function() {
   return src('src/js/*.js')
