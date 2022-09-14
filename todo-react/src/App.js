7c1e699ec132fca219b3f954bec934a13c2869df
aleks <kulykov@ukr.net>
2022-09-14 16:37:45 +0300

    Add state

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,21 +1,31 @@
 import logo from './assets/logo.svg';
 import './styles/App.css';
+
+import React, { useState } from 'react';
+
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 function App() {
-  return (
-    <div className="App">
-        <h2>My Tasks</h2>
-        <Card style={{ width: '50%', margin: '0 auto' }}>
-            <Card.Header as="h5"><Button variant="primary">Add task</Button></Card.Header>
-            <Card.Body>
-                <Card.Title>All tasks</Card.Title>
-                Tasks
-            </Card.Body>
-        </Card>
-    </div>
-  );
+    
+    const [count, setCount] = useState(0);
+    const incrementCount = () => setCount(count +1);
+    
+    return (
+        <div className="App">
+            <h2>My Tasks</h2>
+            <Card style={{ width: '50%', margin: '0 auto' }}>
+                <Card.Header>
+                    <h3 className="float-start">All tasks</h3>
+                    <Button variant="primary" className="float-end" onClick={incrementCount}>Add task</Button>
+                </Card.Header>
+                <Card.Body>
+                    
+                    Tasks count: {count}
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
